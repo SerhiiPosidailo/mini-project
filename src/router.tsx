e401e4ad1352a96bf9f89e59f07bbd4ac6442927
Notebook <zekserj@ukr.net>
2023-11-12 15:56:43 +0200

    Finish

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
 import {createBrowserRouter, Navigate} from "react-router-dom";
+import React from "react";
+
 import {MainLayout} from "./layouts/MainLayout";
 import {MoviesPage} from "./pages/MoviesPage/MoviesPage";
 import {GenresPage} from "./pages/GenrePage/GenresPage";
 import {MovieInfoPage} from "./pages/MovieInfoPage/MovieInfoPage";
-
-import React from "react";
 import {GenresIdPage} from "./pages/GenresIdPage/GenresIdPage";
 import {SearchPage} from "./pages/SearchPage/SearchPage";
 
